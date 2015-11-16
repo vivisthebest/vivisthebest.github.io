3c5ca9796efcada4acba21a91bb6cd14bf0a6332
veev <vivyee@bu.edu>
2015-11-16 18:36:03 -0500

    no contact form

--- contact-veev.php
+++ contact-veev.php
@@ -21,47 +21,5 @@
 	<div class="contactinfo">
 		<p>Please feel free to email me for any project requests or inquiries. I can be contacted through my email (vivyee@bu.edu) or my <a href="www.linkedin.com/in/vivianayee">LinkedIn</a>.</p>
 
-	<section class="message">
-		<form method="post" action="send.php" name = "submit">
-            <?php if (!(isset($_POST['done']))) {?>
-			<label>name</label>
-			<input name="name" placeholder="type here...">
-
-			<label>email</label>
-			<input name="email" type="email" placeholder="type here...">     
-
-			<label>message</label>
-			<textarea name="message" placeholder="type here..."></textarea>
-
-			<label>*What is 2+2? (Antispam)</label>
-			<input name="human" placeholder="answer...">
-
-			<input id="send" name="send" type="submit" value="Submit">
-
-            <?php 
-            } else {
-            $done = $_POST['done'];
-
-            if ($done == 1) { ?>
-                <div class="alerts"><p>message sent</p></div>
-            <?php } else if ($done == 2) { ?>
-                <div class="alerts"><p>error...try again</p></div>
-            <?php } else if ($done == 3) { ?>
-                <div class="alerts"><p>go learn maff stoopid</p></div>
-            <?php } else { ?>
-                <div class="alerts"><p>fill all fields please</p></div>
-            <?php } 
-            }
-            exit;
-            ?>
-
-
-		</form>        
-	</section>
-
-
-	<footer class="form">
-	</footer>
-
 	</div>
 	</html>
